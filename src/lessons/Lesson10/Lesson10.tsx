988ea83fbb16063e14ad7e45074f1f80093ed6c9
MischkaPCHI <michellejung4002@gmail.com>
2024-04-30 12:21:20 +0200

    redact Homework10

--- src/lessons/Lesson10/Lesson10.tsx
+++ src/lessons/Lesson10/Lesson10.tsx
@@ -1,46 +1,40 @@
 import { useEffect, useState } from "react";
+import { v4 } from "uuid";
 
-import { Lesson10StyledContainer, ScrollContainer, ButtonContainer } from "./styles";
+import { Lesson10StyledContainer, ScrollContainer, ButtonContainer, FactContainer } from "./styles";
 import Button from "components/Button/Button";
 
 const Lesson10 = () => {
+  const [catFacts, setCatFacts] = useState<string[]>([]);
 
-    const [catFact, setCatFacts] = useState<string[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+  const fetchCatFacts = async () => {
+    try {
+      const response = await fetch("https://catfact.ninja/fact");
+      if (!response.ok) {
+        throw new Error("Failed fetch");
+      }
+      const body = await response.json();
+      setCatFacts((prevFacts) => [...prevFacts, body.fact]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const serverWork = async () => {
-        try{
-            const response = await fetch('https://catfact.ninja/fact');
-            if (!response.ok) {
-                throw new Error('Failed fetch')
-            }
-            const body = await response.json();
-            setCatFacts(prevFacts => [...prevFacts, body.fact + ' ']);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+  const arrayCatFacts = catFacts.map((prevCatFact: string) => <FactContainer key={v4()}>{prevCatFact}</FactContainer>);
 
-    useEffect(() => {
-        if (isLoading) {
-            serverWork();
-        }
-    },[isLoading]);
-    
+  useEffect(() => {
+    fetchCatFacts();
+  },[]);
 
-    return (
-        <Lesson10StyledContainer>
-            <ScrollContainer>{catFact}</ScrollContainer>
-            <ButtonContainer>
-                <Button name="GET MORE INFO" onButtonClick={() => setIsLoading(true)}>GET MORE INFO</Button>
-                <Button name="DELETE ALL DATA" onButtonClick={() => setCatFacts([])}>DELETE ALL DATA</Button>
-            </ButtonContainer>
-            
-        </Lesson10StyledContainer>
-    )
-
-}
+  return (
+    <Lesson10StyledContainer>
+      <ScrollContainer>{arrayCatFacts}</ScrollContainer>
+      <ButtonContainer>
+        <Button name="GET MORE INFO" onButtonClick={fetchCatFacts}>GET MORE INFO</Button>
+        <Button name="DELETE ALL DATA" onButtonClick={() => setCatFacts([])}>DELETE ALL DATA</Button>
+      </ButtonContainer>
+    </Lesson10StyledContainer>
+  );
+};
 
 export default Lesson10;
