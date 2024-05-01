a50c5ae0e178a962426d8b7f7e84068af0404326
MischkaPCHI <michellejung4002@gmail.com>
2024-05-01 16:07:10 +0200

    Homework11

---
+++ src/components/EmployeeForm/EmployeeForm.tsx
@@ -0,0 +1,66 @@
+import { useFormik } from "formik";
+
+import { EmployeeFormValues } from "./types";
+import { EmployeeFormComponent, EmployeeFormName, InputsContainer } from "./styles";
+import Input from "components/Input/Input";
+import Button from "components/Button/Button";
+
+const EmployeeForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      age: "",
+      jobTitle: "",
+      termsOfUse: false,
+    } as EmployeeFormValues,
+    onSubmit: (values: EmployeeFormValues) => {
+      console.log(values);
+    },
+  });
+  return (
+    <EmployeeFormComponent onSubmit={formik.handleSubmit}>
+      <EmployeeFormName>Employee Form</EmployeeFormName>
+
+      <InputsContainer>
+        <Input
+          name="firstName"
+          type="text"
+          placeholder="Enter your first name"
+          label="frist name:"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+        />
+        <Input
+          name="lastName"
+          type="text"
+          placeholder="Enter your last name"
+          label="last name:"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+        />
+        <Input name="age" type="number" placeholder="Enter your age" label="age:" onChange={formik.handleChange} value={formik.values.age} />
+        <Input
+          name="jobTitle"
+          type="text"
+          placeholder="Enter your job title"
+          label="job title:"
+          onChange={formik.handleChange}
+          value={formik.values.jobTitle}
+        />
+        <Input
+          name="termsOfUse"
+          type="checkbox"
+          placeholder="Enter your terms of use"
+          label="job title:"
+          onChange={formik.handleChange}
+          value={formik.values.termsOfUse.toString()}
+        />
+
+        <Button type="submit" name="Login">Submit</Button>
+      </InputsContainer>
+    </EmployeeFormComponent>
+  );
+};
+
+export default EmployeeForm;
